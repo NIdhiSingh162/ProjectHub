b1fc19fb9c1cfa7139546231556a750a4ad96f8e
Nidhi <2k23.it2310550@gmail.com>
2026-06-19 11:11:18 +0530

    Fix CORS headers manually

--- server/index.js
+++ server/index.js
@@ -1,17 +1,25 @@
 const express = require("express");
-const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
 const projectRoutes = require("./routes/projectRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 
 require("dotenv").config();
-
-const db = require("./db");
+require("./db");
 
 const app = express();
 
-app.use(cors());  
-// app.options("*", cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
