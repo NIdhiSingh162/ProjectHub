d283c5c016968f107ade7e378193625150df418d
Nidhi <2k23.it2310550@gmail.com>
2026-06-21 22:36:48 +0530

    Final CORS fix

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cors = require("cors");
+
 const userRoutes = require("./routes/userRoutes");
 const projectRoutes = require("./routes/projectRoutes");
 const taskRoutes = require("./routes/taskRoutes");
@@ -8,34 +10,28 @@ require("./db");
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+const corsOptions = {
+  origin: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
-
-  next();
-});
+app.use(cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("ProjectHub Backend Running Updated");
+});
+
 app.get("/test-cors", (req, res) => {
-  res.json({ message: "CORS test working" });
+  res.json({ message: "CORS working" });
 });
 
 app.use("/api/users", userRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
-
-
-app.get("/", (req, res) => {
-  // res.send("ProjectHub Backend Running");
-  res.send("ProjectHub Backend Running Updated");
-});
-
 
 const PORT = process.env.PORT || 5000;
 
